fa3297fd4c8bf9ea1f5510a89823653ef40539a8
Matteo <m_mountain@outlook.com>
2023-01-17 11:22:43 +0000

    Initial Commit

---
+++ CarCustomiser/Car.swift
@@ -0,0 +1,16 @@
+//
+//  Car.swift
+//  CarCustomiser
+//
+//  Created by Matteo Mountain on 15/01/2023.
+//
+
+import Foundation
+
+struct Car {
+    let make: String
+    let model: String
+    var topSpeed: Int
+    var acceleration: Double
+    var handling: Int
+}
--- CarCustomiser/ContentView.swift
+++ CarCustomiser/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let car = Car(make: "Mazda", model: "MX5", topSpeed: 125, acceleration: 7.7, handling: 5)
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+        Text("""
+             Make: \(car.make)
+             Model: \(car.model)
+             Top Speed: \(car.topSpeed)mph
+             Acceleration (0-60): \(String(format: "%.1f", car.acceleration))s
+             Handling: \(car.handling)
+             """)
+            .offset(x: -60,y: -280)
+
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
